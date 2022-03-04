2db8f3ec53c0953f639205f1845b357ad1b47115
Thanh Binh <thanhbinhgtv@gmail.com>
2022-03-04 16:40:38 +0700

    Modified

--- source-code/JavaScript/demo1.js
+++ source-code/JavaScript/demo1.js
@@ -1,3 +1,12 @@
-!function test() {
-    console.log('test');
+
+!function Log(){
+    setTimeout(() => {
+        console.log(1);
+        setTimeout(() => {
+            console.log(1);
+            setTimeout(() => {
+                console.log(1);
+            }, 1000)
+        }, 1000)
+    }, 1000)
 }()
